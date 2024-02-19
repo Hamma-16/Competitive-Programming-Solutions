9f6ecda6860e170de039551da9a7daa0a31dde5a
mohamed salah daouthi <mohamedsalah.daouthi@isimg.tn>
2024-02-19 13:17:22 +0100

    Solution added
    Signed-off-by: mohamed salah daouthi <mohamedsalah.daouthi@isimg.tn>

---
+++ A - Bear and Big Brother.cpp
@@ -0,0 +1,18 @@
+//بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ
+//ּاللَهُمَّ صلِّ وسَلِم وبَارِك على سيدنا محمد
+//--Ɛ>TAMATEM<3-- LOADING...
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, y = 0;
+    cin>>a>>b;
+    while(a<=b){
+        a*=3;
+        b*=2;
+        y+=1;
+    }
+    cout<<y<<endl;
+    return 0 ;
+}
