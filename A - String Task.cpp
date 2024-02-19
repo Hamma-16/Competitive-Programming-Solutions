936fdb8b1d3ff1b85828223ee8aeed203d896339
mohamed salah daouthi <mohamedsalah.daouthi@isimg.tn>
2024-02-19 13:57:00 +0100

    Solution added
    Signed-off-by: mohamed salah daouthi <mohamedsalah.daouthi@isimg.tn>

--- A - String Task.cpp
+++ A - String Task.cpp
@@ -12,38 +12,14 @@ int main()
     transform(ch.begin(),ch.end(),ch.begin(),::tolower);
 
     for(int i =0;i<ch.length();i++){
-//بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ
-//ּاللَهُمَّ صلِّ وسَلِم وبَارِك على سيدنا محمد
-//--Ɛ>TAMATEM<3-- LOADING...
 
-#include <bits/stdc++.h>
-        using namespace std;
-
-        int main()
-        {
-            string ch,chres="";
-            cin>>ch;
-            transform(ch.begin(),ch.end(),ch.begin(),::tolower);
-
-            for(int i =0;i<ch.length();i++){
-
-                if(ch[i] == 'a' || ch[i] == 'o' || ch[i] == 'y' || ch[i] == 'e' || ch[i] == 'u' || ch[i] == 'i'){
-                    continue;
-                }
-                else
-                    chres+=".";
-                chres += ch[i];
-            }
-
-            cout<<chres<<endl;
-            return 0;
-        }
         if(ch[i] == 'a' || ch[i] == 'o' || ch[i] == 'y' || ch[i] == 'e' || ch[i] == 'u' || ch[i] == 'i'){
             continue;
         }
-        else
+        else{
             chres+=".";
-        chres += ch[i];
+            chres += ch[i];
+        }
     }
 
     cout<<chres<<endl;
