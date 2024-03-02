7e653ab23be1b6ff5d69198d837fb94525e2329f
Mohamed salah Daouthi <132788921+Hamma-16@users.noreply.github.com>
2024-03-02 02:14:24 +0100

    Add files via upload

---
+++ A - Elephant.cpp
@@ -0,0 +1,17 @@
+//بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ
+//ּاللَهُمَّ صلِّ وسَلِم وبَارِك على سيدنا محمد
+//--Ɛ>TAMATEM<3-- LOADING...
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long;
+#define nl "\n";
+
+
+int main() {
+    int x;
+    cin >> x;
+    int steps = x / 5 + (x % 5 != 0);
+    std::cout << steps << nl;
+    return 0;
+}
